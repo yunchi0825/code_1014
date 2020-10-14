66ba24673ebd5cc06088a085503121de98993708
yunchi0825 <71751221+yunchi0825@users.noreply.github.com>
2020-10-14 15:22:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,12 @@
-input.onGesture(Gesture.LogoUp, function () {
-    basic.showNumber(1)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showNumber(6)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.ScreenDown, function () {
-    basic.showNumber(5)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.FreeFall, function () {
-    basic.showNumber(8)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(0)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.ScreenUp, function () {
-    basic.showNumber(4)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.LogoDown, function () {
-    basic.showNumber(2)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.TiltRight, function () {
-    basic.showNumber(7)
-    basic.pause(100)
-    basic.clearScreen()
-})
-input.calibrateCompass()
 basic.forever(function () {
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.plot(x, y)
-            basic.pause(100)
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(index)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
+        if (index == 5) {
+            break;
         }
     }
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.unplot(x, y)
-            basic.pause(100)
-        }
-    }
+    basic.showIcon(IconNames.Heart)
 })
